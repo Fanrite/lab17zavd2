0c2880f0f69656fdd352f3cf0b1a6fe04816b4ed
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-04-28 21:42:55 +0300

    Добавьте файлы проекта.

---
+++ lab17zavd2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace lab17zavd2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] f;
+
+            if (!File.Exists("D:\\Stytsun.txt"))
+            {
+                MessageBox.Show("Файл кентами 404.\n\n3читування неможливо", "Увага", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            f = File.ReadAllLines("D:\\Stytsun.txt");
+
+            string res = "";
+
+            for (int i = 0; i < f.Length; i++)
+                res += (i + 1).ToString() + ". " + f[i] + "\n";
+
+            MessageBox.Show($"Перелік кентов:\n{res}", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
